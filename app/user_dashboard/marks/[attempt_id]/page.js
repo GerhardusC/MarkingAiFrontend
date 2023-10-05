7686d6b865d9cb3df1a9fbc442f14c3f1b1dcde8
Gerhardus <hardus.cronje@gmail.com>
2023-10-05 15:13:12 +0200

    Provisionally completed second page

--- app/user_dashboard/marks/[attempt_id]/page.js
+++ app/user_dashboard/marks/[attempt_id]/page.js
@@ -61,30 +61,88 @@ const GradesView = ({ params }) => {
               </div>
             );
           })}
-          {/* Selected question */}
-          <div className={styles.questionDetailsContainer}>
-            <h2>Grade 10 Biology Test Term 1</h2>
-            <h5>Question 3</h5>
-            <h1>What is an enzyme?</h1>
+        </div>
+        {/* Selected question */}
+        <div className={styles.questionDetailsContainer}>
+          <h2 className={styles.testName}>Grade 10 Biology Test Term 1</h2>
+          <h5 className={styles.detailedQuestionName}>Question 3</h5>
+          <h1 className={styles.questionText}>What is an enzyme?</h1>
+          <div className={styles.summariesContainer}>
             <details>
-              <summary></summary>
+              <summary>
+                <span>Student answer</span>
+                <span className={styles.summaryDownarrow}>v</span>
+              </summary>
+              <div>
+                <p>
+                  An enzyme is a type of protein that speeds up chemical
+                  reactions in living organisms. It works by attaching to
+                  specific molecules, called substrates, and helping them change
+                  into different molecules. Enzymes make these reactions happen
+                  faster by lowering the energy needed for them. This helps with
+                  vital processes like digestion and energy production in cells.
+                </p>
+              </div>
+            </details>
+            <details>
+              <summary>
+                <span>Marking guide</span>
+                <span className={styles.summaryDownarrow}>v</span>
+              </summary>
               <div>
                 <p></p>
               </div>
             </details>
             <details>
-              <summary></summary>
-              <div>
-                <p></p>
-              </div>
-            </details>
-            <details>
-              <summary></summary>
+              <summary>
+                <span>Model answer</span>
+                <span className={styles.summaryDownarrow}>v</span>
+              </summary>
               <div>
                 <p></p>
               </div>
             </details>
           </div>
+        </div>
+        <div className={styles.gradeContainer}>
+          <div className={styles.headingContainer}>
+            <h2 className={styles.gradingColumnHeading}>Grading</h2>
+            <h2 className={styles.editText}>Edit</h2>
+          </div>
+          <h5 className={styles.detailedQuestionName2}>Question 3</h5>
+          <div className={styles.marksContainer}>
+            <h1>2.0 / 3.0</h1>
+            <p>Marks</p>
+          </div>
+          <details>
+            <summary>
+              <span>Feedback to student</span>
+              <span>v</span>
+            </summary>
+            <div>
+              <p>
+                Great job explaining enzymes and their function! You've clearly
+                described what enzymes are and how they speed up reactions in
+                living organisms. Your mention of enzymes attaching to specific
+                molecules (substrates) and helping them change is spot on.
+              </p>
+              <p>
+                To make your answer even stronger and earn that extra mark,
+                consider adding a bit more detail about how enzymes make
+                reactions happen faster. You could talk about how they create a
+                special environment that encourages chemical changes, or mention
+                their role in breaking down complex substances into simpler
+                ones.
+              </p>
+            </div>
+          </details>
+          <details>
+            <summary>
+              <span>AI Justification for mark</span>
+              <span>v</span>
+            </summary>
+            <div></div>
+          </details>
         </div>
       </div>
     </div>
